b2edcdf0ba9a684e15ec711a1785c5d3aa1710c8
Militant.Saungweme <militant.saungweme@uniscon.com>
2025-03-11 11:52:28 +0100

    setup apollo to handle graphql requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,39 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink, InMemoryCache } from '@apollo/client/core';
 
+function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, '/i18n/', '.json');
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([])),
+    provideApollo(() => {
+      return {
+        link: new HttpLink({
+          uri: 'https://api.github.com/graphql',
+        }),
+        cache: new InMemoryCache(),
+      };
+    }),
+    provideRouter(routes),
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }).providers!,
+  ],
 };
