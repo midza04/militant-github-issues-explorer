b96d7f79eedea86c7d63e0d79bcf7793bcb64bb0
Militant.Saungweme <militant.saungweme@uniscon.com>
2025-03-13 21:10:35 +0100

    cleanup: remove unused translation initialisation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import {
-  HttpClient,
-  provideHttpClient,
-  withInterceptors,
-} from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
 import { InMemoryCache } from '@apollo/client/core';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
-import { inject } from '@angular/core';
 import { HttpLink } from 'apollo-angular/http';
-
-function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient, '/i18n/', '.json');
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -30,13 +23,5 @@ export const appConfig: ApplicationConfig = {
       };
     }),
     provideRouter(routes),
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }).providers!,
   ],
 };
